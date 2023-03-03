ecd02922c8b94b9fc4a36ca2c9c84df06afe407d
yimlaisum2014 <yimlaisum2014@gmail.com>
2023-03-03 15:51:08 +0800

    update Pun sence

--- Assets/Scripts/PUN/NetworkPlayerSpawner.cs
+++ Assets/Scripts/PUN/NetworkPlayerSpawner.cs
@@ -11,9 +11,6 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
     public bool robot_camera_top = true;
     public string Name_RobotCameraTop = "PunRPC_Camera_view";
 
-    public bool robot_camera_lateral = false;
-    public string Name_RobotCameraLateral = "Camera Dope View_2";
-
     public bool robot_object = false;
     public string Name_RobotObjectPose = "Locobot_tf";
 
@@ -33,14 +30,13 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
         if (!firstPlayer)
         {
 
-            spawnPlayerprefab = PhotonNetwork.Instantiate("Human_locobot_pun", transform.position, transform.rotation);
+            spawnPlayerprefab = PhotonNetwork.Instantiate("PunRPC_Human_locobot", transform.position, transform.rotation);
         }
         else
         {
             
             if (robot_real_state) { PhotonNetwork.Instantiate(Name_RobotAvatar, transform.position, transform.rotation); }
             if (robot_camera_top) { PhotonNetwork.Instantiate(Name_RobotCameraTop, new Vector3(0.139f, 0.601f, 3.22f), transform.rotation * Quaternion.Euler(90, 90, -90)); }
-            if (robot_camera_lateral) { PhotonNetwork.Instantiate(Name_RobotCameraLateral, new Vector3(0.139f, 0.601f, 3.22f), transform.rotation * Quaternion.Euler(90, 90, -90)); }
             if (robot_object) { PhotonNetwork.Instantiate(Name_RobotObjectPose, transform.position, transform.rotation); }
         }
     }
