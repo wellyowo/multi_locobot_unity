88d24ffd1dc269b59ba669fde60f944e260c35a2
wellyowo <welly.ee11@nycu.edu.tw>
2023-09-19 21:39:28 +0800

    Update: change start function to Awake to set all connector IP before start program

--- Assets/Scripts/ROS-sharp-extension/Update_rosip.cs
+++ Assets/Scripts/ROS-sharp-extension/Update_rosip.cs
@@ -8,11 +8,19 @@ public class Update_rosip : MonoBehaviour
     public GameObject[] desired_object;
     public string updated_rosip;
 
-    void Start()
+    void Awake()
     {
         for (int i =0; i < desired_object.Length; i++)
         {
-            desired_object[i].GetComponent<RosConnector>().RosBridgeServerUrl = updated_rosip;
+            try
+            {
+                desired_object[i].GetComponent<RosConnector>().RosBridgeServerUrl = updated_rosip;
+            }
+            catch
+            {
+                Debug.Log("No RosConnector component found in " + desired_object[i].name);
+            }
+            
         }
     }
 
